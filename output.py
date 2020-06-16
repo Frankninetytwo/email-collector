28f29f357f394f76e75560e0daf10d50bd4e6197
Frank S <frankninetytwo@gmail.com>
2020-06-16 09:07:03 +0200

    edited comment regarding its incompleteness

--- output.py
+++ output.py
@@ -1,3 +1,10 @@
+'''
+This will be left incomplete for now as the entire project
+just served the purpose to warm up with python and
+web interaction.
+'''
+
+
 class Output:
     def __init__(self, urls, emails):
         self.fPath = 'output.txt'
